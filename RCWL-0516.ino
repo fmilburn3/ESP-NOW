f46499334d6aa3518bb0ebfc64c197d6f56c27a4
Frank Milburn <fmilburn3@users.noreply.github.com>
2018-04-18 20:00:43 -0700

    Update RCWL-0516.ino
    Measured voltage on OUT pin is 3.36 Volts.  Removed requirement for voltage divider in comments.

--- RCWL-0516.ino
+++ RCWL-0516.ino
@@ -1,24 +1,12 @@
 /*
  * ESP8266_RCWL-0516
- * 
- * Note: The ESP8266 is not 5V tolerant but the RCL-0516 requires 5V!
- * Logic level conversioin (e.g. voltage divider) must be used on
- * the output pin of the RCWL-0516 sensor.
- * 
+ *  
  * Pin and wiring connections:
  * RCWL-0516       Wemos D1 Mini
  * ---------       -------------
  * Vcc             5V         
- * OUT             D5   !!Voltage Divider Required!!
+ * OUT             D5 
  * GND             GND
-
- * Voltage Divider
- * RCWL-0516         Wemos D1 Mini
- *  OUT -- 10k Ohm --+--  D5     
- *                   |
- *                15k Ohm
- *                   |
- *                  GND
  *                  
  * Frank Milburn  May 2018
  */
